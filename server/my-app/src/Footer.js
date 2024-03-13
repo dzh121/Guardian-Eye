7593b8781062574c61e03898cc4ceed1f5524b6b
daniel_laptop <danielzivharel@gmail.com>
2024-03-13 19:17:44 +0200

    UI Update

--- server/my-app/src/Footer.js
+++ server/my-app/src/Footer.js
@@ -1,14 +1,17 @@
-// Footer.js
 import React from "react";
-import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-    <footer className="footer bg-dark text-white">
-      <Container className="text-center py-3">
-        <p>&copy; {new Date().getFullYear()} My Application</p>
-      </Container>
-    </footer>
+    <div
+      style={{
+        backgroundColor: "#2f4f4f", // Dark slate blue
+        color: "#fff", // Light text color
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      <p>&copy; {new Date().getFullYear()} My Application</p>
+    </div>
   );
 }
 
