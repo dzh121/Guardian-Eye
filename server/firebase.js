56ed20c48a76afe0c98353b3f6beb9deea704644
Dzh121 <danielzivharel@email.com>
2024-03-30 14:09:05 +0300

    Update .gitignore and environment variables

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./***REMOVED***");
+const serviceAccount = require("./admin.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- server/my-app/src/firebase.js
+++ server/my-app/src/firebase.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: ***REMOVED***
-  authDomain: ***REMOVED***
-  projectId: ***REMOVED***
-  storageBucket: ***REMOVED***
-  messagingSenderId: ***REMOVED***
-  appId: ***REMOVED***
-  measurementId: ***REMOVED***
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
